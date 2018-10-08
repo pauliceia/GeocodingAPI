fa9dadd50b0fee8603f44bc3295ab642a56e97b0
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-10-08 16:46:13 -0300

    Python Script

--- scripts/plpgsql.sql
+++ scripts/plpgsql.sql
@@ -4,25 +4,23 @@
 
 --------------------------------------------------
 
-CREATE OR REPLACE FUNCTION saboya_geometry(inputPlaceId integer) 
+CREATE OR REPLACE FUNCTION saboya_geometry(streetId integer, numberPlace Float) 
 	RETURNS text
 AS
 $BODY$
 DECLARE 
-	streetId Integer;
+	inputPlaceId integer;
 	zeroGeom text;
 	streetGeom text;
-	numberPlace Integer;
 	oldGeomPlace text;
 	streetSize Float;
 	startPoint text;
 	newGeom text;
 	newStreetGeom text;
 BEGIN
-	streetId := (SELECT street.id FROM places_pilot_area AS places INNER JOIN streets_pilot_area AS street ON places.id_street = street.id WHERE places.id = inputPlaceId);
+	inputPlaceId := (SELECT places.id AS inputPlaceId FROM places_pilot_area AS places INNER JOIN streets_pilot_area AS street ON places.id_street = street.id WHERE places.number = numberPlace AND street.id = streetId);
 	zeroGeom := (SELECT places.geom FROM places_pilot_area AS places INNER JOIN streets_pilot_area AS street ON places.id_street = street.id WHERE places.number = 0 AND street.id = streetId);
 	streetGeom := (SELECT  ST_LineMerge(geom) FROM streets_pilot_area WHERE id = streetId);
-	numberPlace := (SELECT number FROM  places_pilot_area WHERE id = inputPlaceId);
 	oldGeomPlace := (SELECT geom FROM  places_pilot_area WHERE id = inputPlaceId);
 	streetSize := (SELECT ST_Length(ST_Transform(geom, 29100)) FROM streets_pilot_area where id = streetId);
 	startPoint := (SELECT  ST_StartPoint(ST_LineMerge(geom)) FROM streets_pilot_area WHERE id = streetId);
@@ -43,22 +41,9 @@ LANGUAGE plpgsql;
 
 --------------------------------------------------
 
-CREATE OR REPLACE FUNCTION updateSaboya() 
-  RETURNS VOID 
-AS $$
-DECLARE 
-   t_curs cursor for 
-      SELECT * FROM places_pilot_area WHERE first_year > 1931;
-   t_row places_pilot_area%rowtype;
-BEGIN
-    FOR t_row IN t_curs LOOP
-        UPDATE places_pilot_area
-            SET geom = (SELECT ST_SetSRID(saboya_geometry(id), 4326))
-        WHERE current of t_curs;
-    END LOOP;
-END;
-$$ 
-LANGUAGE plpgsql;
+SELECT saboya_geometry(289,172) AS saboya_geometry
+
+--------------------------------------------------
 
 --(b) Corrigir as coordenadas (x,y) de todos os places que não estão
 --no sistema métrico e que estão na mesma localização dos places
---
+++ scripts/read_table.py
@@ -0,0 +1,78 @@
+#Imports
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+
+#Postgres connection
+con = psycopg2.connect(host="localhost",database="db_pauliceia", user="postgres", password="teste")
+cur = con.cursor()
+
+#Dataframe Open
+df = pd.read_csv('entrada/tabelao.csv')
+
+#Create new collumns
+df.loc[:,'geom'] = 'null'
+df.loc[:,'first_day'] = 1
+df.loc[:,'first_month'] = 1
+df.loc[:,'first_year'] = 1800
+df.loc[:,'last_day'] = 31
+df.loc[:,'last_month'] = 12
+df.loc[:,'last_year'] = 2000
+
+#id_dict
+id_dict = {}
+
+#For loop
+for i in range(len(df)):
+    if(df['metragem'][i] != True):
+        j = id_dict[df['Id_ponto'][i]]
+        sql = 'SELECT saboya_geometry('+str(df['id_da rua'][i])+', '+str(df['numero'][j])+') AS saboya_geometry;'
+        cur.execute(sql)
+        recset = cur.fetchall()
+        geom = str(recset).replace("[('","").replace("',)]","")
+        df['geom'][i] = geom
+        if (pd.notna(df['data_inicial (DD/MM/AAAA)'][i])):
+            df['first_day'][i] = df['data_inicial (DD/MM/AAAA)'][i].split('/')[0]
+            df['first_month'][i] = df['data_inicial (DD/MM/AAAA)'][i].split('/')[1]
+            df['first_year'][i] = df['data_inicial (DD/MM/AAAA)'][i].split('/')[2]
+        if (pd.notna(df['data_final (DD/MM/AAAA)'][i])):        
+            df['last_day'][i] = df['data_final (DD/MM/AAAA)'][i].split('/')[0]
+            df['last_month'][i] = df['data_final (DD/MM/AAAA)'][i].split('/')[1]
+            df['last_year'][i] = df['data_final (DD/MM/AAAA)'][i].split('/')[2]
+        df['fonte'][i] = '"'+df['fonte'][i]+'"'
+    else:
+        id_dict[df['Id_ponto'][i]] = i
+        sql = 'SELECT saboya_geometry('+str(df['id_da rua'][i])+', '+str(df['numero'][i])+') AS saboya_geometry;'
+        cur.execute(sql)
+        recset = cur.fetchall()
+        geom = str(recset).replace("[('","").replace("',)]","")
+        df['geom'][i] = geom
+        if (pd.notna(df['data_inicial (DD/MM/AAAA)'][i])):
+            df['first_day'][i] = df['data_inicial (DD/MM/AAAA)'][i].split('/')[0]
+            df['first_month'][i] = df['data_inicial (DD/MM/AAAA)'][i].split('/')[1]
+            df['first_year'][i] = df['data_inicial (DD/MM/AAAA)'][i].split('/')[2]
+        if (pd.notna(df['data_final (DD/MM/AAAA)'][i])):        
+            df['last_day'][i] = df['data_final (DD/MM/AAAA)'][i].split('/')[0]
+            df['last_month'][i] = df['data_final (DD/MM/AAAA)'][i].split('/')[1]
+            df['last_year'][i] = df['data_final (DD/MM/AAAA)'][i].split('/')[2]
+        df['fonte'][i] = '"'+df['fonte'][i]+'"'
+
+#Drop columns
+df = df.drop(['logradouro', 'metragem','data_final (DD/MM/AAAA)', 'data_inicial (DD/MM/AAAA)', 'Id_ponto'], axis=1)
+
+#Rename columns
+df = df.rename(columns={'id_da rua': 'id_street', 'numero': 'number', 'numero original':'original_n', 'fonte':'source', 'autor_da_alimentacao':'autor', 'Data':'date'})
+
+#Print df
+print(df)
+
+#Save df
+df.to_csv('saida/new.csv')
+
+#Create con
+engine = create_engine(
+    'postgresql+psycopg2://postgres:teste@localhost:5432/db_pauliceia'
+)
+
+#Run SQL
+df.to_sql('places_pilot_area2', con=engine, schema='public')
