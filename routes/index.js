1cc1c7ae43627f956b4d8eb98fce7155d87b9de8
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2018-10-16 14:07:58 -0300

    Fix Path

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@
 /*--------------------------------------------------+
 | Var                                               |
 +--------------------------------------------------*/
-  var webServiceAddress = process.env.PORT ? "http://localhost:"+process.env.PORT : "http://localhost:3000";
-  //var webServiceAddress = "http://pauliceia.dpi.inpe.br";
+  //var webServiceAddress = process.env.PORT ? "http://localhost:"+process.env.PORT : "http://localhost:3000";
+  var webServiceAddress = "http://pauliceia.dpi.inpe.br";
   var express = require('express');
   var router = express.Router();
   var GeoJSON = require('geojson');
