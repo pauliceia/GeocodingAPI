28df6cfdc17a184ace68cf36e6480bb7521663ac
Rodrigo M. Mariano <rodmariano13@gmail.com>
2018-10-29 11:57:36 -0300

    Updated name of default settings

--- routes/index.js
+++ routes/index.js
@@ -25,9 +25,9 @@ var request = require('request');
 const pg = require('pg');
 
 const db_user = process.env.DATABASE_USER || "postgres";
-const db_pass = process.env.DATABASE_PASS || "teste";
+const db_pass = process.env.DATABASE_PASS || "postgres";
 const db_host = process.env.DATABASE_HOST || "localhost";
-const db_name = process.env.DATABASE_NAME || "db_pauliceia";
+const db_name = process.env.DATABASE_NAME || "pauliceia";
 
 const connectionString = {
     host: db_host,
