cb543ce56cb15ff2be249f3e3e72b64d2117a910
Igor Augusto <igoraugusto1029@gmail.com>
2023-12-03 19:02:38 -0300

    corrige places

--- routes/index.js
+++ routes/index.js
@@ -122,68 +122,47 @@ router.get('/placeslist', (req, response, next) => {
 /*-----------------------------------------------+
 | Places Dataset                                 |
 +-----------------------------------------------*/
-router.get('/places', (req, res, next) => {
-
+router.get('/places', (req, response, next) => {
     //Results Variable
     const results = [];
 
-    //Get a Postgres client from the connection pool
-    pg.connect(connectionString, (err, client, done) => {
+    //Build the SQL Query
+    const SQL_Query_Select_List = "select a.id as places_id, a.id_street, b.name as name_s, a.number::float, a.first_year::integer as firstyear, a.last_year::integer as lastyear, ST_AsText(a.geom) as geom from streets_pilot_area as b join places_pilot_area2 as a on a.id_street::integer = b.id::integer union select a.id as places_id, a.id_street, b.name as name_s, a.number::float, a.first_year::integer as firstyear, a.last_year::integer as lastyear, ST_AsText(a.geom) as geom from streets_pilot_area as b join places_pilot_area as a on a.id_street::integer = b.id::integer where a.number::float = 0.0 order by number;";
 
-        //Handle connection errors
-        if (err) {
-            done();
-            console.log(err);
-            return res.status(500).json({
-                success: false,
-                data: err
+    //Execute SQL Query
+    client.query(SQL_Query_Select_List)
+        .then(res => {
+            const results = res.rows.map(row => {
+                if (!row.lastyear) {
+                    return {
+                        places_id: row.places_id,
+                        id_street: row.id_street,
+                        street_name: row.name_s,
+                        place_name: '',
+                        place_number: row.number,
+                        place_firstyear: row.firstyear,
+                        place_lastyear: 2000,
+                        place_geom: row.geom
+                    };
+                } else {
+                    return {
+                        places_id: row.places_id,
+                        id_street: row.id_street,
+                        street_name: row.name_s,
+                        place_name: '',
+                        place_number: row.number,
+                        place_firstyear: row.firstyear,
+                        place_lastyear: row.lastyear,
+                        place_geom: row.geom
+                    };
+                }
             });
-        }
-
-        //Build the SQL Query
-        const SQL_Query_Select_List = "select a.id as places_id, a.id_street, b.name as name_s, a.number::float, a.first_year::integer as firstyear, a.last_year::integer as lastyear, ST_AsText(a.geom) as geom from streets_pilot_area as b join places_pilot_area2 as a on a.id_street::integer = b.id::integer union select a.id as places_id, a.id_street, b.name as name_s, a.number::float, a.first_year::integer as firstyear, a.last_year::integer as lastyear, ST_AsText(a.geom) as geom from streets_pilot_area as b join places_pilot_area as a on a.id_street::integer = b.id::integer where a.number::float = 0.0 order by number;";
-        //const SQL_Query_Select_List = "select b.id, b.name as name_s, a.name as name_p, a.number, a.first_year as firstyear, a.last_year as lastyear, ST_AsText(a.geom) as geom from streets_pilot_area as b join places_pilot_area2 as a on a.id_street = b.id where a.first_year >= 1 and a.last_year >= 1 order by number;";
-
-        //Execute SQL Query
-        const query = client.query(SQL_Query_Select_List);
-
-        //Push Results
-        query.on('row', (row) => {
-            if (!row.lastyear){
-                results.push({
-                    places_id: row.places_id,
-                    id_street: row.id_street,
-                    street_name: row.name_s,
-                    place_name: '',
-                    place_number: row.number,
-                    place_firstyear: row.firstyear,
-                    place_lastyear: 2000,
-                    place_geom: row.geom
-                });
-            }
-            else {
-                results.push({
-                    places_id: row.places_id,
-                    id_street: row.id_street,
-                    street_name: row.name_s,
-                    place_name: '',
-                    place_number: row.number,
-                    place_firstyear: row.firstyear,
-                    place_lastyear: row.lastyear,
-                    place_geom: row.geom
-                });
-            }
+            return response.json(results);
+        })
+        .catch(err => {
+            console.error('Error executing query', err.stack);
+            return response.json(err.stack)
         });
-
-        //After all data is returned, close connection and return results
-        query.on('end', () => {
-            done();
-
-            //Resuts
-            return res.json(results);
-
-        });
-    });
 });
 
 /*-----------------------------------------------+
--- test/route_index_test.js
+++ test/route_index_test.js
@@ -17,15 +17,14 @@ describe('/GET placeslist', () => {
     });
 });
 
-// describe('/GET places', () => {
-//     it('it should GET all the places', (done) => {
-//         chai.request(server)
-//             .get('/api/geocoding/places')
-//             .end((err, res) => {
-//                 res.should.have.status(200);
-//                 res.body.should.be.a('array');
-//                 //res.body.length.should.be.eql(0);
-//                 done();
-//             });
-//     });
-// });
+describe('/GET places', () => {
+    it('it should GET all the places', (done) => {
+        chai.request(server)
+            .get('/api/geocoding/places')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            });
+    });
+});
