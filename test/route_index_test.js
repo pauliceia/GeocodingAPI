77c07fb3541b33fa60bf9f20b46c4a63da3dcc8f
Igor <igoraugusto1029@gmail.com>
2023-12-22 16:12:09 -0300

    correção nos testes

--- test/route_index_test.js
+++ test/route_index_test.js
@@ -4,36 +4,53 @@ const server = require('../app');
 const mocha = require('mocha');
 const suite = mocha.suite;
 const test = mocha.test;
-const assert = chai.assert;
+const assert = chai.assert
 
 chai.use(chaiHttp);
 
 suite('Route index tests', function () {
-    test('Get placeslist', function(){
+    test('Get placeslist', function(done){
         chai.request(server)
             .get('/api/geocoding/placeslist')
             .end(function(err, res){
                 assert.equal(res.status, 200);
-                assert.typeOf(res.body, 'array');
+                done();
             });
     });
 
-    test('Get places', function (){
+    test('Get places', function (done){
         chai.request(server)
             .get('/api/geocoding/places')
             .end(function(err, res){
                 assert.equal(res.status, 200);
                 assert.typeOf(res.body, 'array');
+                done();
             });
     })
 
-    test('Get streets', function (){
+    test('Get streets', function (done){
         chai.request(server)
             .get('/api/geocoding/streets')
             .end(function(err, res){
                 assert.equal(res.status, 200);
                 assert.typeOf(res.body, 'array');
+                done();
             });
     })
 
+    suite('Geolocation tests', function (done) {
+        const parameters = ['alameda barao de piracicaba,34,1908']
+        parameters.forEach( parameter => {
+            test(`Get geolocation for ${parameter}`, function (done) {
+                chai.request(server)
+                    .get(`/api/geocoding/geolocation/${parameter}/json`)
+                    .end(function (err, res) {
+                        assert.equal(res.status, 200);
+                        assert.typeOf(res.body, 'array');
+                        assert.equal(res.body[1][0].status, 1)
+                        done();
+                    });
+            });
+        });
+    });
 });
--- test/user_index_test.js
+++ test/user_index_test.js
@@ -8,12 +8,13 @@ const test = mocha.test;
 
 chai.use(chaiHttp);
 suite('User API tests', function () {
-    test('Get user', function(){
+    test('Get user', function(done){
         chai.request(server)
             .get('/users')
             .end(function(err, res){
                 assert.equal(res.status, 200);
                 assert.equal(res.text, 'respond with a resource');
+                done();
             });
     })
 });
