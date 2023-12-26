c97c38b03c7acfe36e74a776cfda37b5dfa4b62a
Igor <igoraugusto1029@gmail.com>
2023-12-26 17:17:03 -0300

    altera tipos de variaveis

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 +---------------------------------------------------+
 |Var
 +---------------------------------------------------+*/
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var cors = require('cors');
-var app = express();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const cors = require('cors');
+const app = express();
+app.disable('x-powered-by');
 
 /*  
 +---------------------------------------------------+
@@ -63,7 +63,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -74,7 +74,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
