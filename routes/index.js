c2d85ba378b29fe15b8fb0809ddd5051010c7b8a
Igor <igoraugusto1029@gmail.com>
2023-12-26 16:17:04 -0300

    remove importaçõa inutilizadas

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,6 @@
 // read dotenv
 require('dotenv').config();
 
-
 // default address
 let webServiceAddress = process.env.WEB_SERVICE_HOST ? process.env.WEB_SERVICE_HOST : "https://pauliceia.unifesp.br"
 
@@ -27,46 +26,8 @@ console.log('webServiceAddress: ', webServiceAddress)
 
 var express = require('express');
 var router = express.Router();
-var Search = require('../controllers/searchPoint');
-var Locate = require('../controllers/lineLocate');
-var Create = require('../controllers/lineSubstring');
 var Match = require('../controllers/dictionary');
-var Calculate = require('../controllers/confidenceRate');
 const indexController = require('../controllers/indexRouteController');
-var request = require('request');
-
-/*--------------------------------------------------+
-| Connection                                        |
-+--------------------------------------------------*/
-const pg = require('pg');
-
-const db_user = process.env.DATABASE_USER || "postgres";
-const db_pass = process.env.DATABASE_PASS || "postgres";
-const db_host = process.env.DATABASE_HOST || "localhost";
-const db_name = process.env.DATABASE_NAME || "pauliceia";
-
-const connectionString = {
-    host: db_host,
-    port: 5432,
-    user: db_user,
-    database: db_name,
-    password: db_pass,
-    connectionTimeoutMillis: 5000 // timeout after 5 seconds
-}
-
-console.log('starting server')
-
-const client = new pg.Client(connectionString);
-
-client.connect((err) => {
-    if(err) {
-        console.error('Failed to connect to the database:', err);
-        return;
-    }
-    console.log('Successfully connected to the database');
-});
-
-
 
 /* ------------------------------------------------------------------------+
 |                                                                          |
